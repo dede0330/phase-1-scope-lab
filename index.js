e1995885710b0fe4f22f89b11c6b72c31b350858
DeAndre <deandre.bloomfield@gmail.com>
2021-10-18 11:33:51 -0700

    Done

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// Write your solution in this file!
- var customerName = 'bob'
- const leastFavoriteCustomer = 'Some intial value'
+var customerName = 'bob'
+const leastFavoriteCustomer = 'some initial value'
+var bestCustomer;
 
- function upperCaseCustomerName() {
+function upperCaseCustomerName() {
   customerName = customerName.toUpperCase()
-
+  
   //notice that there is no return statement
   //if you try to use `return customerName.toUpperCase()`, it will return 'BOB', but will not redefine the variable!
 }
